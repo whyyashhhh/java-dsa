4d6676d2eeeaea37f3f189a9901b4086ebf3fd84
whyyashhhh <tsuyashdl@gmail.com>
2025-04-06 00:33:18 +0530

    INFINITE LOOPS

--- loops.java
+++ loops.java
@@ -2,7 +2,11 @@ public class loops {
     
     public static void main(String[] args) {
         //counter ++ => counter = counter +1
-        for(int counter = 0; counter < 3; counter = counter + 1) {
+        // for(int counter = 0; counter < 3; counter = counter + 1) {
+        //     System.out.println("hello world");
+            
+        // }
+        for( int i = 1; i <= 10; i = i*2){
             System.out.println("hello world");
         }
     }
