5d6fa5494c4d54839ffa31a2e284680bcc6f2e60
whyyashhhh <tsuyashdl@gmail.com>
2025-04-09 10:00:11 +0530

    pyramid pattern

---
+++ PyramidPattern.java
@@ -0,0 +1,14 @@
+public class PyramidPattern {
+    public static void main(String[] args) {
+        
+        int n = 4;
+    // outer loop
+    for(int i = 1; i<=n; i++) {
+        // inner loop
+        for(int j = 1; j <=i; j++){
+            System.err.print("*");
+        }
+        System.out.println();
+    }
+    }
+}
